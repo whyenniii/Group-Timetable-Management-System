623b9da06b9059f174e034b221e896d7a46d52ca
Boyeon-Shin <anna007096@gmail.com>
2025-04-18 14:37:56 +0900

    initial commit

---
+++ src/main/java/dbclass/grouptimetablemanagementsystem/GroupTimetableManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package dbclass.grouptimetablemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupTimetableManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupTimetableManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dbclass/grouptimetablemanagementsystem/GroupTimetableManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dbclass.grouptimetablemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupTimetableManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
